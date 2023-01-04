27985688c22365866f38c1d5e6481154f0ba0d3b
Qidam Qolby <110288597+qidamqolby@users.noreply.github.com>
2023-01-04 11:26:02 +0700

    Update main.go

--- server/main.go
+++ server/main.go
@@ -40,5 +40,5 @@ func main() {
 	var port = os.Getenv("PORT")
 
 	fmt.Println("server running localhost:" + port)
-	http.ListenAndServe("localhost:"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
+	http.ListenAndServe(":"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
 }
